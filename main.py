9a60cb58d82493a99acaca4f153c5d84453ba4bc
Oksana-Duda <oksana.duda.25@kse.org.ua>
2026-04-14 01:57:35 +0300

    Зробила графік

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('random_walk.csv')
 df['distance'] = (df['x']**2 + df['y']**2)**0.5
@@ -9,3 +10,16 @@ print(max_len, avg_len)
 max_table = df[df['distance']>avg_len]
 print(max_table)
 max_table.to_json('filtered_walk.json', orient='records', lines=True)
+
+
+plt.figure(figsize=(8,4))
+plt.plot(df['x'], df['y'], color='green', label=r"Траєкторія")
+plt.title("Траєкторія блукання")
+plt.scatter(0, 0, color='red', s=100, label="Початок")
+end_x = df['x'][max(df['step'])]
+end_y = df['y'][max(df['step'])]
+plt.scatter(end_x, end_y, color='blue', s=100, label="кінець")
+plt.xlabel('координата по х')
+plt.ylabel('координата по y')
+plt.legend()
+plt.show()
