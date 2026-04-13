9dfe9302ff64e72cd2e59bbecf94e0ea54b6f80c
Oksana-Duda <oksana.duda.25@kse.org.ua>
2026-04-14 01:33:00 +0300

    Рахую і виводжу середню і максимальну відстані

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ import numpy as np
 
 df = pd.read_csv('random_walk.csv')
 df['distance'] = (df['x']**2 + df['y']**2)**0.5
+max_len = max(df['distance'].values)
+avg_len = df['distance'].sum() / (max(df['step'])+1)
+print(max_len, avg_len)
