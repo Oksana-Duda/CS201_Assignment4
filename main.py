a2d04eb7ea2672996826c7122c4430bcdb439c0f
Oksana-Duda <oksana.duda.25@kse.org.ua>
2026-04-14 01:40:52 +0300

    Відфільтрувала найдовші відстані в таблицю, зберегла в джейсоні

--- main.py
+++ main.py
@@ -6,3 +6,6 @@ df['distance'] = (df['x']**2 + df['y']**2)**0.5
 max_len = max(df['distance'].values)
 avg_len = df['distance'].sum() / (max(df['step'])+1)
 print(max_len, avg_len)
+max_table = df[df['distance']>avg_len]
+print(max_table)
+max_table.to_json('filtered_walk.json', orient='records', lines=True)
